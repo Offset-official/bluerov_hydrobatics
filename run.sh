fb2f2f4e8a863446d7e3ebceff8adf85d9dd8b7e
jalan-subham <subham.jalan@plaksha.edu.in>
2025-05-04 20:36:13 +0530

    yeah subham needed some cool shell automation

---
+++ run.sh
@@ -0,0 +1,7 @@
+python3 scripts/train_simple_point.py A2C --n-envs 12
+python3 scripts/train_simple_point.py A2C --n-envs 12
+python3 scripts/train_simple_point.py A2C --n-envs 12
+python3 scripts/train_simple_point.py A2C --n-envs 12
+python3 scripts/train_simple_point.py A2C --n-envs 12
+python3 scripts/train_simple_point.py A2C --n-envs 12
+python3 scripts/train_simple_point.py A2C --n-envs 12
